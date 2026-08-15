6e5e9502bb19196e93df2f60452f4a1d13113683
dxofficials000 <aliezedavid000@gmail.com>
2026-08-15 19:42:14 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+/* ============================================================
+   FIREBASE CONFIG — shared by artisan-login.html & artisan-dashboard.html
+   ============================================================ */
+const firebaseConfig = {
+  apiKey: "AIzaSyAlHyZuB0MwjNNmukVfVv-t4iRiNSxPh7Y",
+  authDomain: "naijafix-3227b.firebaseapp.com",
+  projectId: "naijafix-3227b",
+  storageBucket: "naijafix-3227b.firebasestorage.app",
+  messagingSenderId: "605013760653",
+  appId: "1:605013760653:web:38e3bb29ba17cc72e5e0c4",
+  measurementId: "G-3WH516R6QH"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
